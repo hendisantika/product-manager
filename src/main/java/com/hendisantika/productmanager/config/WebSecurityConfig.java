644bff9172a344980dc5fb6476f6e20199bc5370
Hendi Santika <hendisantika@gmail.com>
2021-08-05 06:09:39 +0700

    #19 - Add userDetailsService & passwordEncoder bean in WebSecurityConfig

--- src/main/java/com/hendisantika/productmanager/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/productmanager/config/WebSecurityConfig.java
@@ -1,8 +1,12 @@
 package com.hendisantika.productmanager.config;
 
+import com.hendisantika.productmanager.service.UserDetailsServiceImpl;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,4 +20,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
