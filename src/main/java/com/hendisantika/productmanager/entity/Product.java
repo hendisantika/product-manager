bbbb9cd78e8609b2f721ca01f7509f2f7750670f
Hendi Santika <hendisantika@gmail.com>
2021-08-05 05:59:27 +0700

    #7 - Add Product Entity

---
+++ src/main/java/com/hendisantika/productmanager/entity/Product.java
@@ -0,0 +1,35 @@
+package com.hendisantika.productmanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : product-manager
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/08/21
+ * Time: 05.58
+ */
+@Entity
+@Table(name = "products")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String brand;
+    private String madeIn;
+    private float price;
+}
