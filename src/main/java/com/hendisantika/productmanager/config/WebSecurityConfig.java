82aff332278e208f8548aded5fdd86ce38173cfe
Hendi Santika <hendisantika@gmail.com>
2021-08-05 06:08:41 +0700

    #18 - Add WebSecurityConfig

---
+++ src/main/java/com/hendisantika/productmanager/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.hendisantika.productmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : product-manager
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/08/21
+ * Time: 06.07
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
