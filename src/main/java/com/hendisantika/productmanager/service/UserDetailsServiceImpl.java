0112fd8adbc3173287330ab889c214ee766d51b3
Hendi Santika <hendisantika@gmail.com>
2021-08-05 06:04:37 +0700

    #14 - Add UserDetailsServiceImpl

---
+++ src/main/java/com/hendisantika/productmanager/service/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.hendisantika.productmanager.service;
+
+import com.hendisantika.productmanager.entity.User;
+import com.hendisantika.productmanager.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : product-manager
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 05/08/21
+ * Time: 06.03
+ */
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        User user = userRepository.getUserByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException("Could not find user");
+        }
+
+        return new MyUserDetails(user);
+    }
+}
